366f5cc2f4b862ba836318bd66aa165bfe10dcf1
fadhil <965216@telkom.co.id>
2020-02-22 20:09:05 +0700

    first changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,9 @@
             <div class="panel panel-default">
                 <div class="panel-heading">@lang('quickadmin.qa_dashboard')</div>
 
-                <div class="panel-body">
-                    @lang('quickadmin.qa_dashboard_text')
+                <div class="panel-body fad-panel-body">
+                    <p><strong>@lang('quickadmin.qa_dashboard_text')</strong></p>
+                    <img class="fad-das-img" src="{{ asset('images/logo_umb.png') }}" />
                 </div>
             </div>
         </div>
