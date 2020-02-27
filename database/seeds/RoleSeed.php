2ff9990959054fb97f7e8354d9f2d8293a89e010
Fadhil <agungfadhil23@gmail.com>
2020-02-28 00:29:15 +0700

    update dashboard

--- database/seeds/RoleSeed.php
+++ database/seeds/RoleSeed.php
@@ -14,9 +14,10 @@ class RoleSeed extends Seeder
         $items = [
             
             ['id' => 1, 'title' => 'Administrator (can create other users)',],
-            ['id' => 2, 'title' => 'Simple user',],
-            ['id' => 3, 'title' => 'Premium user', 'price' => 9.99, 'stripe_plan_id' => 'premium',],
-
+            ['id' => 2, 'title' => 'Manager IT',],
+            ['id' => 3, 'title' => 'Staff IT',]
+            // ['id' => 2, 'title' => 'Simple user',],
+            // ['id' => 3, 'title' => 'Premium user', 'price' => 9.99, 'stripe_plan_id' => 'premium',],
         ];
 
         foreach ($items as $item) {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,17 @@
                 <div class="panel-heading">@lang('quickadmin.qa_dashboard')</div>
 
                 <div class="panel-body fad-panel-body">
-                    <p><strong>@lang('quickadmin.qa_dashboard_text')</strong></p>
-                    <img class="fad-das-img" src="{{ asset('images/logo_umb.png') }}" />
+                    <p><strong>Hello and welcome, {{{Auth::user()->name}}}</strong></p>
+                    <!-- <p><strong>@lang('quickadmin.qa_dashboard_text')</strong></p> -->
+                    <div>
+                      <img class="fad-das-img" src="{{ asset('images/logo_umb.png') }}" />
+                      <p>Berikut ketentuan yang wajib dipahami pengguna :</p>
+                      <ul>
+                        <li>Aplikasi LaStore dijalankan berdasarkan kesepakatan dan regulasi perusahaan yang berlaku</li>
+                        <li>Diwajibkan untuk tidak memberitahu hak kredensial pengguna pada siapapun termasuk kepada pihak admin.</li>
+                        <li>Segala tindak pelanggaran akan dikenakan hukuman sesuai dengan kebijakan yang berlaku.</li>
+                      </u;>
+                    </div>
                 </div>
             </div>
         </div>
